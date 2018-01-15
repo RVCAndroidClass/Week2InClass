20a4c258c22ac24b1483ebe7d11d2fabfe416476
Chuck Konkol <ckonkol@gmail.com>
2018-01-14 21:17:15 -0600

    Initial commit

---
+++ app/src/main/java/edu/rvc/student/week2assignments/MainActivity.kt
@@ -0,0 +1,40 @@
+package edu.rvc.student.week2assignments
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btnSubmit = findViewById<Button>(R.id.btnSubmit)
+        var txtName = findViewById<EditText>(R.id.txtName)
+
+        btnSubmit.setOnClickListener{
+            val userinput = txtName.text.toString()
+            txtShow.setText(userinput)
+            txtName.setText("")
+            hideKeyboard()
+        }
+    }
+
+    // function to hide keyboard goes right before the last right bracket of Class MainActivity
+//import android.content.Context
+//import android.view.inputmethod.InputMethodManager
+    fun hideKeyboard() {
+        try {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        } catch (e: Exception) {
+            // TODO: handle exception
+        }
+
+    }
+}
